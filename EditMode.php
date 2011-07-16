7fc2942f3869e871521888d48bf816f7c5e9393a
onedayitwillmake <mariogonzalez@gmail.com>
2011-07-16 13:05:55 -0400

    Cleaned up editpage

--- EditMode.php
+++ EditMode.php
@@ -1,39 +1,31 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>ChuClone</title>
-    <meta charset="utf-8">
-    <style type="text/css">
-        body {
-            font-family: Monospace;
-            background-color: #FFFFFF;
-            margin: 0px;
-            overflow: hidden;
-        }
-    </style>
-    <link  href="http://fonts.googleapis.com/css?family=Orbitron:400,500,700,900&v1" rel="stylesheet" type="text/css" >
+    <meta charset="utf-8"/>
+    <title>ChuClone - Alpha</title>
+    <link rel="stylesheet" href="assets/css/960/reset.css"/>
+    <link rel="stylesheet" href="assets/css/960/text.css"/>
+    <link rel="stylesheet" href="assets/css/960/960.css"/>
+    <link rel="stylesheet" href="assets/css/ChuClone.css"/>
+
+    <link href="http://fonts.googleapis.com/css?family=Jura:300,400,500,600&v1" rel="stylesheet" type="text/css">
 
     <?php
-        include("build.php");
+    include("build.php");
     ?>
+
     <script type="text/javascript">
         var game = new ChuClone.ChuCloneGame();
     </script>
-
-    <!--<script type="text/javascript">-->
-        <!--var _gaq = _gaq || [];-->
-        <!--_gaq.push(['_setAccount', 'UA-1249241-1']);-->
-        <!--_gaq.push(['_trackPageview']);-->
-
-        <!--(function() {-->
-            <!--var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;-->
-            <!--ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';-->
-            <!--var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);-->
-        <!--})();-->
-    <!--</script>-->
 </head>
 <body>
-    <div id="guiContainer"></div>
-    <div id="HUDTime"></div>
+<div class="container_12">
+    <div class="clear"></div>
+    <div id="gameContainer" class="grid_12"></div>
+    <div id="editorContainer" class="grid_12" style="height: 540px;"></div>
+	<div class="clear"></div>
+
+	    <div class="grid_4 lineBorder" id="HUDTime" style="margin-top:-10px">00.0 secs</div>
+</div>
 </body>
 </html>
