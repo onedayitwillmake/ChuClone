4ce5b9610b2976ded35fd064fb125e115206f5ad
Mario Gonzalez <mariogonzalez@gmail.com>
2011-07-27 13:53:06 -0400

    Cleaned up MovingPlatformComponent, before adding Y axis

--- js/ChuClone/components/MovingPlatformComponent.js
+++ js/ChuClone/components/MovingPlatformComponent.js
@@ -62,6 +62,8 @@ Abstract:
 	ChuClone.namespace("ChuClone.components");
 	ChuClone.components.MovingPlatformComponent = function() {
 		ChuClone.components.MovingPlatformComponent.superclass.constructor.call(this);
+        this._direction = new b2Vec2(1, 1);
+        this._speed = new b2Vec2(0,0);
 		this.requiresUpdate = true;
 	};
 
@@ -75,10 +77,20 @@ Abstract:
 
 		/**
 		 * Speed that this platform moves left or right
-		 * @type {Number}
+		 * @type {Box2D.Common.Math.b2Vec2}
 		 */
-		_speed	: 3,
-        _direction: 1,
+		_speed	: null,
+
+        /**
+         * Direction this platform is moving in. Can be -1, or 1
+         * @type {Box2D.Common.Math.b2Vec2}
+         */
+        _direction: null,
+
+        /**
+         * No longer used?
+         * @type {Number}
+         */
 		_offset : 0,
 
 		/**0 
@@ -86,12 +98,10 @@ Abstract:
 		 */
 		_initialPosition	: null,
 
-		_angle	: 0,
-
 		/**
 		 * Overwrite to allow component specific GUI
 		 */
-		_editableProperties: {rangeX: {value: 0, max: 0.1, min: 30}, rangeY: {value: 0, max: 0.1, min: 30}, speed: {value: 0, max: 0.2, min: 0}, offset: {value: 0, max: Math.PI, min: -Math.PI}, active: true},
+		_editableProperties: {rangeX: {value: 0, min: 0, max: 30}, rangeY: {value: 0, min: 0, max: 30}, speed: {value: 1, min: 0, max: 15}, offset: {value: 0,  min: -Math.PI,  max: Math.PI}, active: true},
 
 		/**
 		 * @inheritDoc
@@ -99,47 +109,14 @@ Abstract:
 		attach: function( anEntity ) {
 			ChuClone.components.MovingPlatformComponent.superclass.attach.call( this, anEntity );
 
-            this._angle = 0;
-			this._range = this._range || new b2Vec2(this._editableProperties.rangeX, this._editableProperties.rangeY);
-			this._offset = this._offset || this._offset;
+            this._initialPosition = this.attachedEntity.getBody().GetPosition().Copy();
+			this._range = this._range || new b2Vec2(this._editableProperties.rangeX.value, this._editableProperties.rangeY.value);
+            this.reset();
 
-
-			this._initialPosition = this.attachedEntity.getBody().GetPosition().Copy();
-			this.attachedEntity.getBody().SetAwake(true);
-            this.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(this._speed, 0) );
-
-            /**
-             *
-             * @param {Box2D.Dynamics.b2World} b2World
-             */
-            var b = this.attachedEntity.getBody();
-            var that = this;
-            var initialPositionScaled = this._initialPosition;
-            var PTM_RATIO = ChuClone.model.Constants.PTM_RATIO;
-            this.attachedEntity.getBody().drawCustom = function(b2World) {
-                var s = b2World.m_debugDraw.m_ctx;
-                var drawScale = b2World.m_debugDraw.m_drawScale;
-                var rect = initialPositionScaled.Copy();
-
-                /**
-                 * Determine the rectangle extents
-                 */
-                // 1 - then determine the entity width scale down to box2d scale, and then multiply that by the drawscale
-                var extentsHalfWidth = (that.attachedEntity.width/PTM_RATIO+that._range.x)*b2World.m_debugDraw.m_drawScale;
-                var extentsHalfHeight = (that.attachedEntity.height/PTM_RATIO+that._range.y)*b2World.m_debugDraw.m_drawScale;
-
-                // 2 - taking the position + debugOffset and mutliplying by the scale
-                rect.x = (initialPositionScaled.x + b2World.m_debugDraw.offsetX) * b2World.m_debugDraw.m_drawScale;
-                rect.y = (initialPositionScaled.y + b2World.m_debugDraw.offsetY) * b2World.m_debugDraw.m_drawScale;
-                // 3 - Because box2d is center based, move the rectangle -extentsWidth (which is half the width of the rect)
-                rect.x -= extentsHalfWidth;
-                rect.y -= extentsHalfHeight;
-                // 4 - Define the width/height of the rect
-                rect.width = extentsHalfWidth*2;
-                rect.height = extentsHalfHeight*2;
-
-
-                s.fillRect( rect.x, rect.y, rect.width, rect.height )
+            // Draw the extents of the platforms movement if in edit mode
+            if( ChuClone.model.Constants.IS_EDIT_MODE() ) {
+                var that = this;
+                this.attachedEntity.getBody().drawCustom = function( b2World ){ that.drawPlatformForEditor( b2World ) };
             }
 
             __addMovingPlatform( this );
@@ -149,69 +126,99 @@ Abstract:
 		 * Modify the entity to move along axis as defined by sin/cos * range
 		 */
 		update: function() {
+            var finalVelocity = this.attachedEntity.getBody().GetLinearVelocity();
+            var finalPosition = this.attachedEntity.getBody().GetPosition();
 
-            var bodyPosition = this.attachedEntity.getBody().GetPosition();
-            var dist = bodyPosition.x - this._initialPosition.x;//- (this._initialPosition.x + this._range.x)
-            var absDist = Math.abs(dist);
-            var rate = Math.abs( (dist / this._range.x) );
+            var distX = finalPosition.x - this._initialPosition.x;
+            var absDistX = Math.abs(distX);
 
+//            var rate = Math.abs( (dist / this._range.x) );
 //            rate = Math.max(rate, 0.5)
 //            this.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(rate * this._speed * this._direction, 0) );
 
 
-            if( absDist > this._range.x ) {
-                this.attachedEntity.getBody().SetPosition( new b2Vec2(this._initialPosition.x+(this._direction*this._range.x), this._initialPosition.y) );
-                this._direction *= -1;
-                this.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(this._speed * this._direction, 0) );
-
+            if( absDistX > this._range.x ) {
+                finalPosition.x = this._initialPosition.x+(this._direction.x * this._range.x);
+                this._direction.x *= -1;
+                finalVelocity.x = this._speed.x * this._direction.x;
+                this.attachedEntity.getBody().SetLinearVelocity( finalVelocity );
             }
-
-
-//            if( bodyPosition.x > this._initialPosition.x + this._range.x ) {
-//                this.attachedEntity.getBody().SetLinearVelocity( this._speed * this._direction)
-//            }
-//            var angle = Date.now() * 0.001;
-
-
-//            var velocity = new b2Vec2(0, 0);
-//			if( this._range.x !== 0 )
-//				velocity.x = Math.cos(angle + this._offset) * this._range.x;
-//			if( this._range.y !== 0 )
-//				velocity.y = Math.sin(angle + this._offset) * this._range.y;
-
-//            this.attachedEntity.getBody().SetLinearVelocity( velocity );
-//			this._angle += this._speed;
 		},
+
+        /**
+         * Special drawing function while editing that shows the extents of the platforms movements
+         * @param {Box2D.Dynamics.b2World}  Reference to b2World instance
+         */
+        drawPlatformForEditor: function( b2World ) {
+            var initialPositionScaled = this._initialPosition;
+            var PTM_RATIO = ChuClone.model.Constants.PTM_RATIO;
+            var drawScale = b2World.m_debugDraw.m_drawScale;
+
+
+            var rect = initialPositionScaled.Copy();
+
+            /**
+             * Determine the rectangle extents
+             */
+            // 1 - then determine the entity width scale down to box2d scale, and then multiply that by the drawscale
+            var extentsHalfWidth = (this.attachedEntity.width / PTM_RATIO + this._range.x) * b2World.m_debugDraw.m_drawScale;
+            var extentsHalfHeight = (this.attachedEntity.height / PTM_RATIO + this._range.y) * b2World.m_debugDraw.m_drawScale;
+
+            // 2 - taking the position + debugOffset and multiplying by the scale
+            rect.x = (initialPositionScaled.x + b2World.m_debugDraw.offsetX) * b2World.m_debugDraw.m_drawScale;
+            rect.y = (initialPositionScaled.y + b2World.m_debugDraw.offsetY) * b2World.m_debugDraw.m_drawScale;
+            // 3 - Because box2d is center based, move the rectangle -extentsWidth (which is half the width of the rect)
+            rect.x -= extentsHalfWidth;
+            rect.y -= extentsHalfHeight;
+            // 4 - Define the width/height of the rect
+            // extentsHalfWidth multiplied by 2 in draw call below
+
+
+            // Draw into the debugdraw context
+            b2World.m_debugDraw.m_ctx.fillRect( rect.x, rect.y, extentsHalfWidth * 2, extentsHalfHeight * 2 )
+        },
 
         /**
          * Restore material and restitution
          */
         detach: function() {
             __removeMovingPlatform( this );
-			this.attachedEntity.getBody().SetPosition( this._initialPosition.Copy() );
+
+            // Reset the attached body
+            this.attachedEntity.getBody().SetPosition( this._initialPosition.Copy() );
             this.attachedEntity.getBody().drawCustom = null;
             this.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(0, 0) );
 
+            // Clear our properties
 			this._initialPosition = null;
+            this._range = null;
+
 			ChuClone.components.MovingPlatformComponent.superclass.detach.call(this);
         },
 
-		/**
+        /**
+         * Set the '_editableProperties' object to our values
+         */
+        setEditableProps: function() {
+			this._editableProperties.rangeX.value = this._range.x;
+			this._editableProperties.rangeY.value = this._range.y;
+			this._editableProperties.speed.value = this._speed.x;
+			this._editableProperties.offset.value = this._offset ;
+            this._editableProperties.active = this.requiresUpdate;
+        },
+
+        /**
 		 * @inheritDoc
 		 */
 		onEditablePropertyWasChanged: function() {
-
-
-
 			this._range.x = this._editableProperties.rangeX.value;
 			this._range.y = this._editableProperties.rangeY.value;
-			this._speed = this._editableProperties.speed.value;
+			this._speed.x = this._editableProperties.speed.value;
+			this._speed.y = this._editableProperties.speed.value;
 			this._offset = this._editableProperties.offset.value;
 
-			var wasActive = this.requiresUpdate;
-			var isActive = this._editableProperties.active;
+            this.reset();
 
-            return;
 //			// Reset the body
 //			if( this._speed != previousSpeed || this._offset != previousOffset ) {
 //				this.attachedEntity.getBody().SetPosition( this._initialPosition.Copy() );
@@ -227,25 +234,15 @@ Abstract:
 //			}
 
 //            this.reset();
-			this.requiresUpdate = isActive;
+			this.requiresUpdate = this._editableProperties.active;
 		},
 
         reset: function() {
-            //this.attachedEntity.getBody().SetAwake(true);
-//            this._angle = 0
-            this.attachedEntity.getBody().SetPosition( this._initialPosition.Copy() );
-            this.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(0,0))
-        },
+            var startPosition = this._initialPosition.Copy();
 
-        /**
-         * Set the '_editableProperties' object to our values
-         */
-        setEditableProps: function() {
-			this._editableProperties.rangeX.value = this._range.x;
-			this._editableProperties.rangeY.value = this._range.y;
-			this._editableProperties.speed.value = this._speed;
-			this._editableProperties.offset.value = this._offset ;
-            this._editableProperties.active = this.requiresUpdate;
+			this.attachedEntity.getBody().SetAwake(true);
+            this.attachedEntity.getBody().SetPosition( startPosition );
+            this.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(this._speed.x*this._direction.x,0) );
         },
 
         /**
@@ -255,8 +252,8 @@ Abstract:
             var returnObject = ChuClone.components.MovingPlatformComponent.superclass.getModel.call(this);
             returnObject.initialPosition = {x: this._initialPosition.x, y: this._initialPosition.y};
             returnObject.range = {x: this._range.x, y: this._range.y};
-            returnObject.speed = this._speed;
-            returnObject.offset = this._offset;
+            returnObject.offset = {x: this.offset.x, y: this.offset.y};
+            returnObject.speed = this._speed.x;
             return returnObject;
         },
 
@@ -267,8 +264,8 @@ Abstract:
             ChuClone.components.MovingPlatformComponent.superclass.fromModel.call(this, data);
 
             this._range = new b2Vec2( data.range.x, data.range.y );
-			this._speed = data.speed;
-			this._offset = data.offset;
+            this._offset = new b2Vec2( data.offset.x, data.offset.y );
+			this._speed = new b2Vec2( data.speed, data.speed );
         },
 
         RESET_ALL_PLATFORMS_EXCEPT: function( exception ) {
@@ -281,7 +278,6 @@ Abstract:
 
                 platform.attachedEntity.getBody().SetPosition( platform._initialPosition.Copy() );
 //				platform.attachedEntity.getBody().SetLinearVelocity( new b2Vec2(0, 0) );
-                platform._angle = 0;
             }
         }
 
